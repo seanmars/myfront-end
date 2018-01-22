87105c2fef57aea99260c06e9eb3728484a2e845
Mars <mars0222@gmail.com>
2018-01-22 15:33:03 +0800

    Create MathUtils.js

---
+++ js/MathUtils.js
@@ -0,0 +1,5 @@
+// toFixed with rounding, using the RegExp
+function toFixed(num, fixed) {
+    var re = new RegExp('^-?\\d+(?:\.\\d{0,' + (fixed || -1) + '})?');
+    return num.toString().match(re)[0];
+}

b21cd61e15741a63dd92516afc38bc113814c487
Mars <mars0222@gmail.com>
2019-10-23 14:01:54 +0800

    Update MathUtils.js

--- js/MathUtils.js
+++ js/MathUtils.js
@@ -3,3 +3,18 @@ function toFixed(num, fixed) {
     var re = new RegExp('^-?\\d+(?:\.\\d{0,' + (fixed || -1) + '})?');
     return num.toString().match(re)[0];
 }
+
+// random int between min(included) and max(included)
+function randomInt(min, max) {
+    if (min == max) {
+        return min;
+    } else (min > max)
+    {
+        let t = max;
+        max = min;
+        min = t;
+    }
+
+    range = max - min + 1;
+    return Math.floor(Math.random() * Math.floor(range)) + min;
+};
